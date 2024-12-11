801872d037f2e688a125fa1e7c11bd3dc2d3050a
JessenRamadeksaAllen <chenallen244@gmail.com>
2024-12-11 23:41:56 +0700

    BISMILLAH FIX

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,10 +29,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        $page_data = Page::where('id',1)->first();
         $room_data = Room::get();
          
-        view()->share('global_page_data', $page_data);
+        view()->share('global_page_data');
         view()->share('global_room_data', $room_data);
         
 
